9a26cc68ffba2f4041388e7da803cbd0316e34f8
Olemis Lang <olemis+work@gmail.com>
2019-01-13 14:25:02 -0500

    [cx] Import CX homebrew rule from BigOokie/homebrew-skycoin-cx tap

---
+++ Formula/skycoin-cx.rb
@@ -0,0 +1,35 @@
+# MIT License
+# 
+# Copyright (c) 2019 BigOokie
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+class SkycoinCx < Formula
+    desc "Skycoin CX is a general purpose, interpreted and compiled programming language, with a very strict type system and a syntax similar to Go(lang). CX provides a new programming paradigm based on the concept of affordances."
+    homepage "https://github.com/skycoin/cx"
+    url "https://github.com/skycoin/cx/files/2588185/cx-0.5.17-bin-macos-x64.zip"
+    sha256 "ef0dadb7f2b92479dbc5c59ac608bf73c033cbe4e5c78e5875e3580e324192ec"
+    version "0.5.17"
+  
+    bottle :unneeded
+  
+    def install
+      bin.install "cx"
+    end
+end
