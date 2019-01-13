c3e21d56ad0141a62c8b190b111bc68f2c160b29
Olemis Lang <olemis+work@gmail.com>
2019-01-13 15:16:23 -0500

    [ci] refs #1 - Define version before SHA1 in skycoin-cx formula
    Reported in https://travis-ci.org/simelo/homebrew-skycoin/builds/479124735#L207-L209

--- Formula/skycoin-cx.rb
+++ Formula/skycoin-cx.rb
@@ -24,8 +24,8 @@ class SkycoinCx < Formula
   desc "Skycoin CX is a general programming language based on affordances"
   homepage "https://github.com/skycoin/cx"
   url "https://github.com/skycoin/cx/files/2588185/cx-0.5.17-bin-macos-x64.zip"
-  sha256 "ef0dadb7f2b92479dbc5c59ac608bf73c033cbe4e5c78e5875e3580e324192ec"
   version "0.5.17"
+  sha256 "ef0dadb7f2b92479dbc5c59ac608bf73c033cbe4e5c78e5875e3580e324192ec"
 
   bottle :unneeded
 
