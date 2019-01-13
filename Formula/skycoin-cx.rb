11910f1976fb9548fc6609611257a858e0c40f36
Olemis Lang <olemis+work@gmail.com>
2019-01-13 15:05:39 -0500

    [ci] refs #1 - Remove trailing whitespace in skycoin-cx formula

--- Formula/skycoin-cx.rb
+++ Formula/skycoin-cx.rb
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2019 BigOokie
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,9 +26,9 @@ class SkycoinCx < Formula
   url "https://github.com/skycoin/cx/files/2588185/cx-0.5.17-bin-macos-x64.zip"
   sha256 "ef0dadb7f2b92479dbc5c59ac608bf73c033cbe4e5c78e5875e3580e324192ec"
   version "0.5.17"
-  
+
   bottle :unneeded
-  
+
   def install
     bin.install "cx"
   end
