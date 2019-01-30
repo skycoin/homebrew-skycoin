80f9c4a530539ebf4a7abc7be91790e6fbb7db31
Olemis Lang <olemis@simelo.tech>
2019-01-30 02:51:48 -0500

    [gcc-arm] refs #11 - Correct SHA for GCC ARM 5.3

--- Formula/gcc-arm-none-eabi-53.rb
+++ Formula/gcc-arm-none-eabi-53.rb
@@ -6,7 +6,7 @@ class GccArmNoneEabi53 < Formula
   url "https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-mac.tar.bz2"
   version "20160330"
   # url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/5_3-2016q1/gccarmnoneeabi532016q120160330mactar.bz2?revision=f8c9bfa2-ae89-470c-a845-5fa423439b47?product=GNU-RM%20Downloads,64-bit,,Mac%20OS%20X,5-2016-q1-update'
-  sha256 "5656cdec40f99d5c054a85bbc694276e1c4a1488cdacbbc448bc6acd3bbe070d"
+  sha256 "480843ca1ce2d3602307f760872580e999acea0e34ec3d6f8b6ad02d3db409cf"
 
   def install
     ohai "Copying binaries..."
