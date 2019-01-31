4591a4992b30ed0f7d491951e4c39d25fbcf9b39
Olemis Lang <olemis@simelo.tech>
2019-01-30 21:31:51 -0500

    [ci] refs #9 - SHA256 for clang-llvm-400

--- Formula/clang-llvm-400.rb
+++ Formula/clang-llvm-400.rb
@@ -5,7 +5,7 @@ class ClangLlvm400 < Formula
   homepage "http://releases.llvm.org/download.html"
   url "http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-apple-darwin.tar.xz"
   version "4.0.0"
-  sha256 "89b776c7cf0591c810b5b60067e4dc113b5b71bc50084a536e71b894a97fdccb"
+  sha256 "5504064916d0651c185ceff85871298f31e2eaf4abf1333b2c36f5eed5e9cde6"
 
   def install
     ohai "Copying binaries..."
