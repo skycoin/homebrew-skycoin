6d931e02f6c6a159939bf02e34b851c8ae896ed2
Olemis Lang <olemis@simelo.tech>
2019-01-30 21:38:46 -0500

    [clang-llvm] refs #9 - version is redundant with version scanned from URL
    reported in https://travis-ci.org/skycoin/homebrew-skycoin/builds/486706989#L211-L232

--- Formula/clang-llvm-501.rb
+++ Formula/clang-llvm-501.rb
@@ -4,7 +4,6 @@ class ClangLlvm501 < Formula
   desc "C language family frontend for LLVM"
   homepage "http://releases.llvm.org/download.html"
   url "http://releases.llvm.org/5.0.1/clang+llvm-5.0.1-x86_64-apple-darwin.tar.xz"
-  version "5.0.1"
   sha256 "89b776c7cf0591c810b5b60067e4dc113b5b71bc50084a536e71b894a97fdccb"
 
   def install
--- Formula/clang-llvm-601.rb
+++ Formula/clang-llvm-601.rb
@@ -4,7 +4,6 @@ class ClangLlvm601 < Formula
   desc "C language family frontend for LLVM"
   homepage "http://releases.llvm.org/download.html"
   url "http://releases.llvm.org/6.0.1/clang+llvm-6.0.1-x86_64-apple-darwin.tar.xz"
-  version "6.0.1"
   sha256 "89b776c7cf0591c810b5b60067e4dc113b5b71bc50084a536e71b894a97fdccb"
 
   def install
