1ba17baf04888a4b4e4a1a68133610ebad3c3e8d
Olemis Lang <olemis@simelo.tech>
2019-02-01 21:11:12 -0500

    [clang-llvm] refs #16 - Another attempt to fix clang-llvm decrease version error
    ... failing again in https://travis-ci.org/skycoin/homebrew-skycoin/builds/487667574

--- Formula/clang-llvm.rb
+++ Formula/clang-llvm.rb
@@ -4,6 +4,8 @@ class ClangLlvm < Formula
   desc "C language family frontend for LLVM"
   homepage "http://releases.llvm.org/download.html"
   url "http://releases.llvm.org/7.0.0/clang+llvm-7.0.0-x86_64-apple-darwin.tar.xz"
+  # TODO: Remove on next clang release
+  version "7.0.1"
   sha256 "b3ad93c3d69dfd528df9c5bb1a434367babb8f3baea47fbb99bf49f1b03c94ca"
 
   def install
